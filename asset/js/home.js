abc7c10186becba4f2f6e0d4a6ec1c7f4cd59852
PandaDev <hoangjay09@gmail.com>
2021-11-30 13:10:07 +0700

    Hoàng Hiếu Trung - Home DOM Js homepage for guest

---
+++ asset/js/home.js
@@ -0,0 +1,359 @@
+/* ========== HOME DATA ========== */
+const homeData = [
+  {
+    contentHeading: "Lựa chọn của Spotify",
+    contentDesc: "",
+    contentBox: [
+      {
+        boxImgSrc: "./asset/img/playlist/hot-hits-playlist.jfif",
+        boxTitle: "Hot hit vietnam",
+        boxDesc:
+          "Đông tới Tây, đây là những ca khúc thịnh hành nhất ở Việt Nam. Ảnh bìa: Vũ.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/vpop-khong-the-thieu.jfif",
+        boxTitle: "Vpop không thể thiếu",
+        boxDesc: "V-Pop đã đơm hoa trên những khúc ca này. Ảnh bìa: Hoàng Dũng",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/pop-rising.jfif",
+        boxTitle: "Pop rising vietnam",
+        boxDesc:
+          "Những ca khúc sẽ thành hit trong nay mai. Ảnh bìa: Hoàng Dũng",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/thien-ha-nghe-gi.jfif",
+        boxTitle: "Thiên hạ nghe gì",
+        boxDesc:
+          "Những gì mà người bên cạnh bạn đang nghe. Ảnh bìa: Alan Walker",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/fresh-finds.jfif",
+        boxTitle: "Fresh finds vietnam",
+        boxDesc:
+          "Khám phá các bài hát đáng nghe từ những cái tên có thể bạn chưa biết. Ảnh bìa: Lương Minh Trang; thiết kế: Isu Kim",
+      },
+    ],
+  },
+  {
+    contentHeading: "Bảng xếp hạng",
+    contentDesc: "",
+    contentBox: [
+      {
+        boxImgSrc: "./asset/img/playlist/top-50-vn.jpg",
+        boxTitle: "50 bài hát hàng đầu tại Việt Nam",
+        boxDesc:
+          "Thông tin cập nhật hằng ngày về những bản nhạc được nghe nhiều nhất hiện nay tại Việt Nam.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/top-50-gl.jpg",
+        boxTitle: "50 bài hát hàng đầu tại Toàn Cầu",
+        boxDesc:
+          "Thông tin cập nhật hằng ngày về những bản nhạc được nghe nhiều nhất hiện nay tại Toàn Cầu.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/hot-hits-playlist.jfif",
+        boxTitle: "Hot hit vietnam",
+        boxDesc:
+          "Đông tới Tây, đây là những ca khúc thịnh hành nhất ở Việt Nam. Ảnh bìa: Vũ.",
+      },
+    ],
+  },
+  {
+    contentHeading: "Tập trung",
+    contentDesc: "Âm nhạc giúp bạn tập trung",
+    contentBox: [
+      {
+        boxImgSrc: "./asset/img/playlist/peaceful-piano.jfif",
+        boxTitle: "Peacefull Piano",
+        boxDesc: "Relax and indugle with beautiful piano pieces",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/music-for-concentration.jfif",
+        boxTitle: "Music For Concentration",
+        boxDesc:
+          "Minimalism, electronica and modern classical to concentrate to.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/intense-studying.jfif",
+        boxTitle: "Intense studying",
+        boxDesc: "Focus-enhancing piano for your study session",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/rain-sounds.jfif",
+        boxTitle: "Rain sounds",
+        boxDesc: "Steady rain without any thunder.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/em-dem.jfif",
+        boxTitle: "Êm đềm",
+        boxDesc: "Thư giãn cùng những giai điệu diệu êm",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/iofi-beats.jfif",
+        boxTitle: "Iofi beats",
+        boxDesc: "Beats to relax, study and focus...",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/peaceful-guitar.jfif",
+        boxTitle: "Peaceful guitar",
+        boxDesc: "Unwind to these ",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/chill-lofi.jfif",
+        boxTitle: "Chill Lofi Study Beats",
+        boxDesc: "The perfect study beats, twenty four seven.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/piano-in-bg.jfif",
+        boxTitle: "Piano in the Background",
+        boxDesc: "A calm piano soundtrack to all activities.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/instrumental-study.jfif",
+        boxTitle: "Instrumental Study",
+        boxDesc: "A soft musical backdrop for your studies.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/jazz-in-bg.jfif",
+        boxTitle: "Jazz in the Background",
+        boxDesc: "Soft jazz for all your activities.",
+      },
+    ],
+  },
+  {
+    contentHeading: "Tâm trạng",
+    contentDesc: "Playlist hợp tâm trạng của bạn.",
+    contentBox: [
+      {
+        boxImgSrc: "./asset/img/playlist/ta-noi-no-zdui.jfif",
+        boxTitle: "Ta nói nó zdui",
+        boxDesc: "Cười như mọi ngày, cày như mọi người. ",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/phut-co-don.jfif",
+        boxTitle: "Phút Cô Đơn Ngày Mưa",
+        boxDesc:
+          "Giọt mưa trên cửa kính gợi ký ức về một thời đã xa. Ảnh bìa: Trung Quân",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/dieu-buon-nhat.jfif",
+        boxTitle: "Điều buồn nhất",
+        boxDesc:
+          "Điều buồn nhất là anh biết lại làm như không biết... Ảnh bìa: T.R.I",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/have-a-great-day.jfif",
+        boxTitle: "Have a great day!",
+        boxDesc: "Feel great with these timelessly fun songs!",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/coffee-and-piano.jfif",
+        boxTitle: "Coffee and piano",
+        boxDesc:
+          "Soft (the quietest of them all), subtle, no words needed. Take a sip and enjoy the music of composers from around the world.",
+      },
+    ],
+  },
+  {
+    contentHeading: "Bản phát hành phổ biến",
+    contentDesc: "",
+    contentBox: [
+      {
+        boxImgSrc: "./asset/img/playlist/music-of-friday-vn.jfif",
+        boxTitle: "New Music Friday Vietnam",
+        boxDesc:
+          "Những chiếc nhạc mới trong tuần, được tuyển chọn cẩn thận. Ảnh bìa: Juky San",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/alan-walker.jfif",
+        boxTitle: "World Of Walker",
+        boxDesc: "Alan Walker",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/keep-it-hush.jfif",
+        boxTitle: "Keep it hush",
+        boxDesc: "Dipha Barus, Afgan, Esther Geraldine",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/sweet-talker.jfif",
+        boxTitle: "Sweet talker",
+        boxDesc: "Years & Years, Galantis",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/meant-2-be.jfif",
+        boxTitle: "Meant 2 be",
+        boxDesc: "Shakira Jasmine, Nuca",
+      },
+    ],
+  },
+  {
+    contentHeading: "Trà hay cà phê?",
+    contentDesc: "",
+    contentBox: [
+      {
+        boxImgSrc: "./asset/img/playlist/hiphop-viet.jfif",
+        boxTitle: "Hip-hop việt",
+        boxDesc:
+          "Dòng chảy của rap Việt luôn cuồn cuộn với các tên tuổi Đen, Da LAB, Binz...",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/tu-man-anh.jfif",
+        boxTitle: "Từ màn ảnh",
+        boxDesc:
+          "Nhạc phim Hometown Cha-Cha-Cha, Chuyện Đời Bác Sĩ (Hospital Playlist), My Name, Yumi's Cells... và những bộ phim khác",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/hot-hits-playlist.jfif",
+        boxTitle: "Hot hit vietnam",
+        boxDesc:
+          "Đông tới Tây, đây là những ca khúc thịnh hành nhất ở Việt Nam. Ảnh bìa: Vũ.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/lofi-chill-thu-gian.jfif",
+        boxTitle: "Lofi chill để thư giãn",
+        boxDesc: "Mấy bài này chill phết",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/nhu-khuc-nhac-chuong.jfif",
+        boxTitle: "Như khúc nhạc chuông",
+        boxDesc:
+          "Nằm cà phê võng lựa nhạc chuông nhạc chờ. Ảnh bìa: Trịnh Đình Quang",
+      },
+    ],
+  },
+  {
+    contentHeading: "Nhạc nền cho ngôi nhà của bạn",
+    contentDesc: "",
+    contentBox: [
+      {
+        boxImgSrc: "./asset/img/playlist/thoai-mai-gac-chan-len.jfif",
+        boxTitle: "Thoải mái gác chân lên",
+        boxDesc: "Nằm nhà, thoải mái gác chân lên nghỉ ngơi...",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/tu-man-anh.jfif",
+        boxTitle: "Từ màn ảnh",
+        boxDesc:
+          "Nhạc phim Hometown Cha-Cha-Cha, Chuyện Đời Bác Sĩ (Hospital Playlist), My Name, Yumi's Cells... và những bộ phim khác",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/dinner-with-friend.jfif",
+        boxTitle: "Dinner with friends",
+        boxDesc: "Dinner with Friends? Here's the perfect playlist.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/sing-in-shower.jfif",
+        boxTitle: "Song to sing in the shower",
+        boxDesc:
+          "Make your shower more uplifting by singing along to these hits.",
+      },
+      {
+        boxImgSrc: "./asset/img/playlist/em-dem.jfif",
+        boxTitle: "Êm đềm",
+        boxDesc: "Thư giãn cùng những giai điệu diệu êm",
+      },
+    ],
+  },
+];
+
+console.log(homeData);
+
+const homeSection = document.querySelector(".home");
+
+homeData.forEach((content) => {
+  const homeContent = document.createElement("div");
+  homeContent.classList.add("mb-32");
+
+  const mb4 = content.contentDesc.length > 0 ? "mb-4" : "";
+
+  const homeContentHeader = document.createElement("div");
+  homeContentHeader.classList.add(
+    "home-content_header",
+    "flex",
+    "flex-between",
+    "mb-16"
+  );
+  homeContentHeader.innerHTML = `
+    <div>
+      <h4 class="${mb4}">
+        <a href="#!" class="home-content_heading text-24-700 text-white h-underline">
+          ${content.contentHeading}
+        </a>
+      </h4>
+      <p class="text-14-400 text-grey">${content.contentDesc}</p>
+    </div>
+  `;
+
+  if (content.contentBox.length >= 5) {
+    const a = document.createElement("a");
+    a.setAttribute("href", "#!");
+    a.classList.add("text-12", "text-white", "text-uppercase", "h-underline");
+    a.textContent = "Xem tất cả";
+    homeContentHeader.appendChild(a);
+  }
+
+  const homeContentList = document.createElement("div");
+  homeContentList.classList.add("home-content_list", "grid", "primary-grid");
+
+  const homeContentListContent = content.contentBox;
+
+  if (homeContentListContent.length > 5) {
+    for (let i = 0; i <= 4; i++) {
+      const homeContentItem = document.createElement("a");
+      homeContentItem.classList.add("box", "p-16");
+      homeContentItem.setAttribute("href", "#!");
+      homeContentItem.innerHTML = `
+            <div class="box-head mb-16">
+                <img
+                    src="${homeContentListContent[i].boxImgSrc}"
+                    alt="Background của playlist"
+                />
+                <button class="box-play flex flex-center text-white">
+                    <ion-icon name="play"></ion-icon>
+                </button>
+            </div>
+            <div class="box-content">
+                <h5 class="box-title text-16 text-white text-capitalize mb-4">
+                    ${homeContentListContent[i].boxTitle}
+                </h5>
+                <p class="box-desc text-14-400 text-grey">
+                    ${homeContentListContent[i].boxDesc}
+                </p>
+            </div>
+        `;
+      homeContentList.appendChild(homeContentItem);
+    }
+  } else {
+    homeContentListContent.forEach((item) => {
+      const homeContentItem = document.createElement("a");
+      homeContentItem.classList.add("box", "p-16", "bg-black-800");
+      homeContentItem.setAttribute("href", "#!");
+      homeContentItem.innerHTML = `
+            <div class="box-head mb-16">
+                <img
+                    src="${item.boxImgSrc}"
+                    alt="Background của playlist"
+                />
+                <button class="box-play flex flex-center text-white">
+                    <ion-icon name="play"></ion-icon>
+                </button>
+            </div>
+            <div class="box-content">
+                <h5 class="box-title text-16 text-white text-capitalize mb-4">
+                    ${item.boxTitle}
+                </h5>
+                <p class="box-desc text-14-400 text-grey">
+                    ${item.boxDesc}
+                </p>
+            </div>
+        `;
+      homeContentList.appendChild(homeContentItem);
+    });
+  }
+
+  homeContent.appendChild(homeContentHeader);
+  homeContent.appendChild(homeContentList);
+
+  homeSection.appendChild(homeContent);
+});
